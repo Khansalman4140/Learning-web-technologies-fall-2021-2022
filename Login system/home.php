699a8d95d9a3bca6b0f8cfe3033aa9dab36ed27d
John Doe <johndoe@example.com>
2021-10-06 15:58:59 -0700

    multi user

--- Login system/home.php
+++ Login system/home.php
@@ -1,3 +1,9 @@
+<?php 
+	session_start();
+	if(isset($_SESSION['flag'])){
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,19 +11,13 @@
 	<title>Home Page</title>
 </head>
 <body>
-	<center>
-		<br>
-		<br>
-		<br>
-		<h4> Welcome to </h4>
-		<h1> HOME PAGE </h1>
-		<br>
-		<br>
-		<a href="login.html"> Log In </a>
-		<br>
-		<br>
-		<a href="reg.html"> Registration </a>
-	</center>
-
+	<h1>Welcome Home!</h1>
+	<a href="logout.php"> logout</a>
 </body>
 </html>
+
+<?php
+	}else{
+		header('location: login.html');
+	}
+?>
